a07787ca5c3d72ff699639f73600f977299557bd
Mário Leal <lealmario47@gmail.com>
2024-02-01 01:13:11 +0000

    empty API keys file

---
+++ apiKeys.py
@@ -0,0 +1,10 @@
+youtubeAPI = "AIzaSyCY9Yn5teb2D4VCK5waAXpbba8k3GKU6uk"
+
+accuWeatherAPI = "0EPyLKhEi5GiZh2oLU6LNT31NBacp95G"
+
+elevenLabsAPI = "dcc83bd97d337b032fd06c3c30f26343"
+
+spotifyID = "eb8e7ddb487e440ca21827c911b10bcc"
+spotifySecret = "6c4485615f8341bda1ac493c6d52ac60"
+spoitfyURL = "https://accounts.spotify.com/authorize"
+
